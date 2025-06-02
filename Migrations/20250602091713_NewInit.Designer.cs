0cbd11857f9536b771675edf58178b45d4c7d223
OutlanderWW <iwo.ropa@gmail.com>
2025-06-02 11:21:17 +0200

    Sending it with modeleed

--- Migrations/20250602091713_NewInit.Designer.cs
+++ Migrations/20250602091713_NewInit.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InvestigationSupportSystem.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250602090834_NewInit")]
+    [Migration("20250602091713_NewInit")]
     partial class NewInit
     {
         /// <inheritdoc />
